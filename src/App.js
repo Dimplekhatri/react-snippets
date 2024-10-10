5db8f237f108716c4526fa722befc190a4aabd5e
Dimplekhatri <121119897+Dimplekhatri@users.noreply.github.com>
2024-10-10 15:16:15 +0530

    Updated App styles and logic, removed unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Charger", quantity: 12, packed: false },
+]
+
+const PackingList=({initialItems})=>{
+  return(
+    <ul>
+      {initialItems.map((item) => (
+        <Items item={item} key={item.id}/>
+      ))
+      }
+    </ul>
+  )
+}
+const Items=({item})=>{
+  return(
+    <li>
+      {item.quantity} {item.description}
+    </li>
+  )
+
+}
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <PackingList initialItems={initialItems}/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
